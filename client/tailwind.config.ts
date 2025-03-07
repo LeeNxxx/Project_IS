ec436350f4186542316be660675f8801befaaa9e
LeeNxxx <pitchayanin1806@gmail.com>
2025-03-07 21:32:02 +0700

    update

---
+++ client/tailwind.config.ts
@@ -0,0 +1,36 @@
+import type { Config } from "tailwindcss";
+export default {
+    darkMode: ["class"],
+    content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+      extend: {
+      keyframes: {
+        typing: {
+          "0%": {
+            width: "0%",
+            visibility: "hidden"
+          },
+          "100%": {
+            width: "100%"
+          }
+        },
+        blink: {
+          "50%": {
+            borderColor: "transparent"
+          },
+          "100%": {
+            borderColor: "white"
+          }
+        }
+      },
+      animation: {
+        typing: "typing 2s steps(20) infinite alternate, blink .7s infinite"
+      }
+    },
+  },
+  plugins: [],
+} satisfies Config;
