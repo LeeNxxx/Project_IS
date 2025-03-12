923a30678322b1fc54cf7afbc17f96d21ad2bb49
LeeNxxx <pitchayanin1806@gmail.com>
2025-03-12 23:21:50 +0700

    img

--- client/app/loading.tsx
+++ client/app/loading.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Loading() {
     return (
         <main className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-            <Image src="/load2.gif" alt="loading" width={150} height={150} className="Loading" />
+            <Image src="/load2.gif" alt="loading" width={130} height={130} className="Loading" />
         </main>
     )
 }
